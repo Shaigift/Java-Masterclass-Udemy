528d5a2cc2a317ea21bdd2fba6faa100d74cd67d
Shaigift <Mphogivenshai@gmail.com>
2020-07-26 22:08:49 +0200

    Update

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main extends Object {
+
+    public static void main(String[] args) {
+
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(40);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+    }
+
+
+
+}
