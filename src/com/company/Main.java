e644bbef599fbaaa14958d565f9bc772da893ef6
Shaigift <Mphogivenshai@gmail.com>
2020-07-27 16:06:41 +0200

    Update

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*Player player = new Player();
+	    player.name = "Mpho";
+	    player.health = 20;
+	    player.weapon = "Sword";
+
+	    int damage = 10;
+	    player.loseHealth(damage);
+	    System.out.println("Remaining health = " + player.healthRemaining());
+
+	    damage = 11;
+	    player.health = 200;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());*/
+		EnhancedPlayer player = new EnhancedPlayer("Mpho", 50, "Sword");
+		System.out.println("Intial health is " + player.getHealth());
+    }
+}
