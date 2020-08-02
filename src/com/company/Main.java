8f7653a544294342e85ffa8881b401f3e16c8e0d
Shaigift <Mphogivenshai@gmail.com>
2020-08-02 21:49:51 +0200

    Update

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import com.company.DeskPhone;
+import com.company.ITelephone;
+import com.company.MobilePhone;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
+
+    }
+}
