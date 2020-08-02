031d0536753ddcd3320205bed69587b1f8b440db
Shaigift <Mphogivenshai@gmail.com>
2020-08-02 13:45:30 +0200

    Update

---
+++ src/com/company/Customer.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double v) {
+        return;
+    }
+}
