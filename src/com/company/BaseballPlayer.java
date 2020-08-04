8b7ae2677c4017bdb67bfdefcdf7980231394842
Shaigift <Mphogivenshai@gmail.com>
2020-08-04 16:44:26 +0200

    Update

---
+++ src/com/company/BaseballPlayer.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.Player;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
